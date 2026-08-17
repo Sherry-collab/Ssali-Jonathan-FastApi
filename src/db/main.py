6cd6b39b03f565ac4d35d5bb13d57b9f1fb1db43
sherry <sherry@Grims-MacBook-Air.local>
2026-08-17 12:55:24 +0500

    Create database with SQLModel

--- src/db/main.py
+++ src/db/main.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import config
 
@@ -8,3 +8,9 @@ engine = AsyncEngine(
     echo= True
 )
 )
+
+async def init_db():
+    async with engine.begin() as conn:
+        from src.books.models import Book
+        
+        await conn.run_sync(SQLModel.metadata.create_all)
