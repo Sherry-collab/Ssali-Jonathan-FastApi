b4a335bcf39458684e73f82eb911bdf7d01fb332
sherry <sherry@Grims-MacBook-Air.local>
2026-08-20 03:16:07 +0500

    Done with modification of Login route

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -8,7 +8,6 @@ class UserCreateModel(BaseModel):
     first_name: str = Field(max_length=25)
     last_name: str = Field(max_length=25)
     password: str = Field(min_length=6)
-    
 class UserModel(BaseModel):
     uid: uuid.UUID 
     username: str
@@ -20,3 +19,6 @@ class UserModel(BaseModel):
     created_at: datetime
     updated_at: datetime 
     
+class UserLoginModel(BaseModel):
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -19,11 +19,11 @@ def generate_passwd_hash(password: str)-> str:
 def verify_passwd(password: str, hash: str) -> bool:
     return passwd_context.verify(password,hash)
 
-def access_token(user_data: dict, expiry: timedelta = None, refresh: bool = False):
+def create_access_token(user_data: dict, expiry: timedelta = None, refresh: bool = False):
     payload = {}
     
     payload['user'] = user_data
-    payload['exp'] = datetime.now + (expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY))
+    payload['exp'] = datetime.now() + (expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY))
     
     payload['jti'] = str(uuid.uuid4())
     
@@ -49,3 +49,4 @@ def decode_token(token: str) -> dict:
     except jwt.PyJWTError as e:
         logging.exception(e)
         return None
+    
