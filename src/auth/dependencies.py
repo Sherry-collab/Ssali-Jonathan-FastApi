4bbd020ed96771d58419ce673015516d232683d3
sherry <sherry@Grims-MacBook-Air.local>
2026-08-25 15:24:55 +0500

    Using Redis to revoke access tokens

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -3,6 +3,7 @@ from fastapi.security import HTTPBearer
 from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 from fastapi.exceptions import HTTPException
+from src.db.redis import token_in_blocklist
 
 class TokenBearer(HTTPBearer):
     
@@ -17,8 +18,17 @@ class TokenBearer(HTTPBearer):
         
         token_data = decode_token(token)
         
-        if not self.token_valid:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid or expired token")
+        if not self.token_valid(token):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail={
+                "error": "This token is invalid or expired",
+                "resolution": "Please get a new token"
+            })
+        
+        if await token_in_blocklist(token_data['jti']):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail={
+                "error": "This token is invalid or has been revoked",
+                "resolution": "Please get a new token"
+            })
         
         self.verify_token_data(token_data)
          
@@ -28,7 +38,7 @@ class TokenBearer(HTTPBearer):
         
         token_data = decode_token(token)
         
-        return True if token_data is not None else False
+        return token_data is not None
     
     def verify_token_data(self, token_data):
         raise NotImplementedError("Please override this method in child classes")
