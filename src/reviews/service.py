e3622f8fc536db2736721b37d68aa29f6796c4b1
sherry <sherry@Grims-MacBook-Air.local>
2026-09-09 01:16:14 +0500

    Create API endpoints for adding a review

--- src/reviews/service.py
+++ src/reviews/service.py
@@ -5,6 +5,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi.exceptions import HTTPException
 from fastapi import status
 from .schemas import ReviewCreateModel
+import logging
 
 book_service = BookService()
 user_service = UserService()
@@ -12,6 +13,7 @@ user_service = UserService()
 class ReviewService:
      
     async def add_review_to_book(
+        self,
         user_email: str,
         book_uid: str,
         review_data: ReviewCreateModel,
@@ -24,6 +26,16 @@ class ReviewService:
             review_data_dict = review_data.model_dump()
             new_review = Review(**review_data_dict)
             
+            if not book:
+                raise HTTPException(
+                    status_code= status.HTTP_404_NOT_FOUND,
+                    detail="Book not found"
+                )
+            if not user:
+                raise HTTPException(
+                        status_code= status.HTTP_404_NOT_FOUND,
+                        detail="User not found"
+                )
             new_review.user = user
             new_review.book = book
             
@@ -31,6 +43,7 @@ class ReviewService:
             await session.commit()
             return new_review
         except Exception as e:
+            logging.exception(e)
             raise HTTPException(
                 status_code= status.HTTP_500_INTERNAL_SERVER_ERROR, detail= "Oops ... Something went wrong"
             )
